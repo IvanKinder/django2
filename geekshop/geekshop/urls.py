743dddffd2e02acef4211d122fca388c27e0c176
Ivan Kinder <ivankinder11@gmail.com>
2021-01-15 13:38:57 +0300

    должно работать

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from mainapp import views as mainapp
 
 
@@ -24,8 +24,3 @@ handler404 = 'mainapp.views.not_found'
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-   import debug_toolbar
-
-   urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
