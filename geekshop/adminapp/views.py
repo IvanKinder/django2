8853e7c1b8f6e3008a58ea41ce896ce41200849a
Ivan Kinder <ivankinder11@gmail.com>
2021-01-26 10:44:24 +0300

    ...

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import user_passes_test
+from django.db.models.expressions import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse, reverse_lazy
@@ -219,6 +220,12 @@ class ProductCategoryUpdateView(UpdateView):
         context['title'] = 'категории/редактирование'
         return context
 
+    def form_valid(self, form):
+        if 'discount' in form.cleaned_data:
+            discount = form.cleaned_data['discount']
+            if discount:
+                self.object.product_set.update(price=F('price') * (1 - discount / 100))
+        return super().form_valid(form)
 
 # @user_passes_test(lambda u: u.is_superuser)
 # def category_delete(request, pk):
