fcabee6a29b17195b254e041d9b1c52988ff16c2
Ivan Kinder <ivankinder11@gmail.com>
2021-01-26 10:27:23 +0300

    ...

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.db import connection
+from django.db.models.expressions import F
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
 
