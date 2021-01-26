b3530a782b6663ed3e5bcac8243737c948960e34
Ivan Kinder <ivankinder11@gmail.com>
2021-01-26 12:33:37 +0300

    ...

--- geekshop/mainapp/tests.py
+++ geekshop/mainapp/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.core.management import call_command
+from django.test import TestCase, Client
 
-# Create your tests here.
+from mainapp.models import ProductCategory, Product
+
+
+class TestMainappSmoke(TestCase):
+
+    def setUp(self):
+        test_category = ProductCategory.objects.create(name='Test')
+        Product.objects.create(
+            category=test_category,
+            name='prosuct test 1'
+        )
+        Product.objects.create(
+            category=test_category,
+            name='prosuct test 2'
+        )
+        self.client = Client()
+
+    def test_mainapp_urls(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    # def tearDown(self):
+    #     pass
