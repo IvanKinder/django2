5e7d840ffb60d36123b2595428b9011649fe5d1e
Ivan Kinder <ivankinder11@gmail.com>
2021-01-27 09:24:35 +0300

    tests

--- geekshop/authapp/tests.py
+++ geekshop/authapp/tests.py
@@ -18,6 +18,7 @@ class TestAuthUserTestCase(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_anonymous)
+        self.assertNotContains(response, 'Пользователь', status_code=200)
 
     def tearDown(self):
         call_command('sqlsequencereset', 'mainapp', 'authapp', 'ordersapp', 'basketapp')
