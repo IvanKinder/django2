5aa3a62b03888291d454a13f2e0a719b943dd9e8
Ivan Kinder <ivankinder11@gmail.com>
2021-01-27 11:40:56 +0300

    tests

--- geekshop/adminapp/templatetags/my_tags.py
+++ geekshop/adminapp/templatetags/my_tags.py
@@ -17,8 +17,9 @@ def media_folder_users(string):
 def media_folder_products(string):
     if not string:
         string = 'products_images/default.png'
+        return f'{settings.MEDIA_URL}{string}'
 
-    return f'{settings.MEDIA_URL}{string}'
+    return f'{settings.MEDIA_URL}products_images/{string}'
 
 
 register.filter('media_folder_users', media_folder_users)
