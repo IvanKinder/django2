3803d97939ac5483b91474148618381ad798279c
Ivan Kinder <ivankinder11@gmail.com>
2021-01-27 10:34:11 +0300

    tests

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -257,7 +257,7 @@ class ProductCategoryDeleteView(DeleteView):
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
         # category_products = Product.objects.filter(category__pk=self.object.pk)
-        category_products = self.object
+        category_products = str(self.object)
 
         with open('log.txt', 'w') as log_file:
             log_file.write(category_products)
