b1b9217bcb073f3958d2c6742709eb7598cbf5a6
Ivan Kinder <ivankinder11@gmail.com>
2021-01-27 09:21:21 +0300

    tests

--- geekshop/authapp/tests.py
+++ geekshop/authapp/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.core.management import call_command
+from django.test import TestCase, Client
 
-# Create your tests here.
+from authapp.models import ShopUser
+
+
+class TestAuthUserTestCase(TestCase):
+
+    def setUp(self):
+        call_command('flush', '--noinput')
+        self.client = Client()
+
+        self.superuser = ShopUser.objects.create_superuser('django', 'django@gb.local', 'geekbrains')
+        self.user = ShopUser.objects.create_user('test1', 'test1@gb.local', 'geekbrains')
+        self.user_with_fn = ShopUser.objects.create_user('test2', 'test2@gb.local', 'geekbrains', first_name='Test2')
+
+    def test_user_login(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_anonymous)
+
+    def tearDown(self):
+        call_command('sqlsequencereset', 'mainapp', 'authapp', 'ordersapp', 'basketapp')
